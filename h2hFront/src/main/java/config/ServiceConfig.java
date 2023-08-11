4c6c2059742042d32949b559c02dde85e4f2b962
ParkIlKwon <123136010+ParkIlKwon@users.noreply.github.com>
2023-08-11 12:36:29 +0900

    프론트 컨피그 수정

--- h2hFront/src/main/java/config/ServiceConfig.java
+++ h2hFront/src/main/java/config/ServiceConfig.java
@@ -9,91 +9,66 @@ import svc.*;
 
 @Configuration
 public class ServiceConfig {
-	
+
 	@Autowired
 	private ServiceSvc serviceSvc;
-	
-	@Autowired
-	private ServiceChartSvc serviceChartSvc;
-	
+
 	@Autowired
 	private ServiceAcceptSvc serviceAcceptSvc;
-	
+
 	@Autowired
 	private ServiceRequestListSvc serviceRequestListSvc;
-	
+
 	@Bean
-	   public ServiceCtrl serviceCtrl() {
-	      ServiceCtrl serviceCtrl = new ServiceCtrl();
-	      serviceCtrl.setServiceSvc(serviceSvc);
-	      return serviceCtrl;
-	   }
-	
-	@Bean
-	public ServiceChartCtrl serviceChartCtrl() {
-	ServiceChartCtrl serviceChartCtrl = new ServiceChartCtrl();
-	serviceChartCtrl.setServiceChartSvc(serviceChartSvc);
-	return serviceChartCtrl;
+	public ServiceCtrl serviceCtrl() {
+		ServiceCtrl serviceCtrl = new ServiceCtrl();
+		serviceCtrl.setServiceSvc(serviceSvc);
+		return serviceCtrl;
 	}
 
 	@Bean
 	public ServiceAcceptCtrl serviceAcceptCtrl() {
-	ServiceAcceptCtrl serviceAcceptCtrl = new ServiceAcceptCtrl();
-	serviceAcceptCtrl.setServiceAcceptSvc(serviceAcceptSvc);
-	return serviceAcceptCtrl;
+		ServiceAcceptCtrl serviceAcceptCtrl = new ServiceAcceptCtrl();
+		serviceAcceptCtrl.setServiceAcceptSvc(serviceAcceptSvc);
+		return serviceAcceptCtrl;
 	}
 
 	@Bean
 	public ServiceRequestListCtrl serviceRequestListCtrl() {
-	ServiceRequestListCtrl serviceRequestListCtrl = new ServiceRequestListCtrl();
-	serviceRequestListCtrl.setServiceRequestListSvc(serviceRequestListSvc);
-	return serviceRequestListCtrl;
+		ServiceRequestListCtrl serviceRequestListCtrl = new ServiceRequestListCtrl();
+		serviceRequestListCtrl.setServiceRequestListSvc(serviceRequestListSvc);
+		return serviceRequestListCtrl;
 	}
 
-	
 	@Bean
 	public ServiceDao serviceDao() {
 		return new ServiceDao(DbConfig.dataSource());
 	}
-	
-	@Bean
-	public ServiceChartDao serviceChartDao() {
-		return new ServiceChartDao(DbConfig.dataSource());
-	}
-	
+
 	@Bean
 	public ServiceAcceptDao serviceAcceptDao() {
 		return new ServiceAcceptDao(DbConfig.dataSource());
 	}
-	
+
 	@Bean
 	public ServiceRequestListDao serviceRequestListDao() {
 		return new ServiceRequestListDao(DbConfig.dataSource());
 	}
-	
-	
-	
+
 	@Bean
 	public ServiceSvc serviceSvc() {
 		ServiceSvc serviceSvc = new ServiceSvc();
 		serviceSvc.setServiceDao(serviceDao());
 		return serviceSvc;
 	}
-	
-	@Bean
-	public ServiceChartSvc serviceChartSvc() {
-		ServiceChartSvc serviceChartSvc = new ServiceChartSvc();
-		serviceChartSvc.setServiceChartDao(serviceChartDao());
-		return serviceChartSvc;
-	}
-	
+
 	@Bean
 	public ServiceAcceptSvc serviceAcceptSvc() {
 		ServiceAcceptSvc serviceAcceptSvc = new ServiceAcceptSvc();
 		serviceAcceptSvc.setServiceAcceptDao(serviceAcceptDao());
 		return serviceAcceptSvc;
 	}
-	
+
 	@Bean
 	public ServiceRequestListSvc serviceRequestListSvc() {
 		ServiceRequestListSvc serviceRequestListSvc = new ServiceRequestListSvc();
